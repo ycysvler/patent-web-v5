f9a37a38bd0dc14706b89eefeb330c22f8743ba2
zhanghongqing@interrcs.com <zhanghongqing@interrcs.com>
2018-06-29 10:49:51 +0800

    + 增加配置文件

---
+++ server/config/config.js
@@ -0,0 +1,37 @@
+/**
+ * 全局配置文件
+ *
+ * Created by zhanghongqing on 2018/6/29.
+ */
+
+module.exports = {
+    // mongodb 相关配置
+    mongodb: {
+        uri: 'mongodb://10.10.22.178/',
+        options: {
+            useMongoClient: true,
+            server: {socketOptions: {keepAlive: 1}},
+            replset: {socketOptions: {keepAlive: 1}}
+        }
+    },
+
+    // mysql 相关配置
+    mysql: {
+        host: '10.10.220.104',
+        port: 9306,
+        user: 'admin',
+        password: 'admin',
+        database: 'URCS_EOS'
+    },
+
+    // redis 相关配置
+    redis: {
+        host: '127.0.0.1',
+        port: 6379
+    },
+
+    // server 相关配置
+    server:{
+        port: 3000                          // 服务启动端口号
+    }
+};
--- server/index.js
+++ server/index.js
@@ -7,16 +7,20 @@ const Koa = require('koa');                             // 引用koa框架
 const consuming = require('./middleware/consuming');    // 加载计算耗时中间件
 const logger = require('./common/logger');              // 引用日志组建
 const loader = require('./routeloader');                // 路由加载器
+const config = require('./config/config');              // 加载配置文件
 
 
 const app = new Koa();                                  // 创建koa实例化
 const log = logger('app');                              // 日志
 
+app.on('error', err=>{
+    log.error({body:err, context:ctx},'server error');  // 全局错误处理
+});
+
 app.use(consuming);                                     // 计算耗时中间件
 app.use(loader.routes()).use(loader.allowedMethods());  // 加载路由
 
 
-let port = 3000;
-app.listen(port);                                       // 启动http服务
+app.listen(config.server.port);                         // 启动http服务
 
-log.info({port:port},'patent v5.0 service is starting at port 3000');
+log.info({port:config.server.port},'patent v5.0 service is starting at port ' + config.server.port);
